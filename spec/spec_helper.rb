abc54b6a719cc0a3a7c1a7d0cb73498f74879177
Ric Farr <ricfarr@sbcglobal.net>
2011-05-15 00:10:57 -0500

    Done with static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'spork'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -25,3 +26,23 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 end
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  Rspec.configure do |config|
+    config.mock_with :rspec
+
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    config.use_transactional_fixtures = true
+  end
+end
+
+Spork.each_run do
+end
+
